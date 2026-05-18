183496b00add2b84c7b0e36e3836f3bce372a6fd
mattrob108 <mattrob108@gmail.com>
2026-05-17 20:22:00 -0600

    Add v2 runtime config with Supabase credentials

---
+++ public/v2/js/config.js
@@ -0,0 +1,7 @@
+// Temenos v2 runtime config.
+window.TEMENOS_V2_CONFIG = {
+  SUPABASE_URL: 'https://isfdktgunaquujmvklcr.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZmRrdGd1bmFxdXVqbXZrbGNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMDk5NTcsImV4cCI6MjA4ODY4NTk1N30.oBKxnWzvKALntk2qP3rg0tSUuFVDlACzfL-upuVhYEo',
+  ASTRO_API_KEY: '',
+  AUTH_REDIRECT: window.location.origin + '/v2/app.html',
+};
